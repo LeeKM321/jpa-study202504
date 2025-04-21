de092658326301defce07ad2656c3a9b93713fff
LeeKM321 <stephen4951@gmail.com>
2025-04-21 15:25:59 +0900

    chap04: 단방향 연관관계 확인, FetchType Eager와 Lazy의 차이 확인

--- src/main/java/com/study/jpa/chap04/repository/EmployeeRepository.java
+++ src/main/java/com/study/jpa/chap04/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.study.jpa.chap04.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
