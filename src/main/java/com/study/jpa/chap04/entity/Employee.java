d0413616e5c93a28e2c3066173a76d340257afb0
LeeKM321 <stephen4951@gmail.com>
2025-04-21 16:29:06 +0900

    chap04: 양방향 관계에서 데이터 갱신이 반대편에 반영되지 않는 문제(연관관계 편의 메서드로 해결)

--- src/main/java/com/study/jpa/chap04/entity/Employee.java
+++ src/main/java/com/study/jpa/chap04/entity/Employee.java
@@ -28,4 +28,20 @@ public class Employee {
     @JoinColumn(name = "dept_id") // FK 컬럼명 (연관 테이블의 컬럼명과 일치하게)
     private Department department;
 
+    // 연관관계 편의 메서드 (양방향에서 연관된 필드가 수정될 경우
+    // 실제 테이블과의 데이터를 맞춰주기 위한 메서드 선언)
+    public void changeDepartment(Department department) {
+        this.department = department;
+        // 사원의 부서가 변경돼? -> 부서쪽 사원 리스트에도 이 객체를 추가해 주자.
+        department.getEmployees().add(this);
+    }
+
+
 }
+
+
+
+
+
+
+
--- src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.study.jpa.chap04.repository;
 
 import com.study.jpa.chap04.entity.Department;
+import com.study.jpa.chap04.entity.Employee;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,34 @@ class DepartmentRepositoryTest {
         System.out.println("\n\n\n");
         System.out.println("department = " + department);
         System.out.println(department.getEmployees());
+        System.out.println("\n\n\n");
+    }
+
+    @Test
+    @DisplayName("양방향 연관관계에서 연관 데이터의 수정")
+    void testChangeDept() {
+        // 1번 사원의 부서를 1 -> 2번 부서로 변경해야 한다.
+
+        // given
+        Employee foundEmp
+                = employeeRepository.findById(3L).orElseThrow();
+        Department department
+                = departmentRepository.findById(1L).orElseThrow();
+
+        // emp리스트에 아무리 사원 add 해봤자 실제 DB에는 적용 안됩니다!
+//        department.getEmployees().add(foundEmp);
+
+        // when
+        // update는 트랜잭션이 종료되어야 동작합니다.
+        // 이 작업 단위(메서드) 안에서 부서에도 사원의 변경된 정보를 사용할 수 있도록
+        // 연관관계 편의 메서드를 통해 리스트에 사원을 직접 추가해서 마치 변경사항이
+        // 바로 반영된 것처럼 사용이 가능.
+        foundEmp.changeDepartment(department);
+
+        // then
+        employeeRepository.save(foundEmp);
+        System.out.println("\n\n\n");
+        department.getEmployees().forEach(System.out::println);
         System.out.println("\n\n\n");
     }
 
